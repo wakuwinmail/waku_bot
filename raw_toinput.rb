432f2e03c070f754a58ba0a6322c4bf930594a97
tembin <tembin0925@gmail.com>
2017-12-22 23:37:45 +0900

    正規化

--- raw_toinput.rb
+++ raw_toinput.rb
@@ -2,11 +2,12 @@ require 'natto'
 
 natto = Natto::MeCab.new#形態素解析ライブラリのオブジェクト作成
 
-input=File.open("input.txt","w")#出力用ファイルを開く
+input=File.open("input.txt","w:utf-8")#出力用ファイルを開く
 
 File.open("rawtext.txt","r:utf-8") do |file|#文章ファイルを開く
   file.each_line do |words|
-    natto.parse(words) do |n|#文章を単語ごとに分割する
+    # 文章を正規化し単語ごとに分割する（natto.parse が Shift JIS じゃないと効かないっぽい？）
+    natto.parse(words.unicode_normalize(:nfkc).encode("shift_jis")) do |n|
       input.print(" #{n.surface}") #出力用ファイルに書き出し
     end
     input.puts("\n")
