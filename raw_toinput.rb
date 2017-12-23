f1692462071705fbc109192dd5beed3a99f68a05
wakuwinmail <wakuwinmail@yahoo.co.jp>
2017-12-23 10:00:05 +0900

    正規化周りのバグ取り
    nattoと相性が悪かったのでwordcutの方で正規化するように変更
    もし原因がわかったら変更するかも

--- raw_toinput.rb
+++ raw_toinput.rb
@@ -2,12 +2,14 @@ require 'natto'
 
 natto = Natto::MeCab.new#形態素解析ライブラリのオブジェクト作成
 
-input=File.open("input.txt","w:utf-8")#出力用ファイルを開く
+input=File.open("input.txt","w")#出力用ファイルを開く
 
 File.open("rawtext.txt","r:utf-8") do |file|#文章ファイルを開く
   file.each_line do |words|
     # 文章を正規化し単語ごとに分割する（natto.parse が Shift JIS じゃないと効かないっぽい？）
-    natto.parse(words.unicode_normalize(:nfkc).encode("shift_jis")) do |n|
+    #natto.parse(words.unicode_normalize(:nfkc).encode("shift_jis")) do |n|
+    #なぜか動かなくてよくわからん。別の方法を探すことにする
+    natto.parse(words) do |n|#文章を単語ごとに分割する
       input.print(" #{n.surface}") #出力用ファイルに書き出し
     end
     input.puts("\n")
--- wordcut.rb
+++ wordcut.rb
@@ -3,7 +3,7 @@ words=File.open("data.txt","w:utf-8")#出力用ファイルを開く
 words.puts("\n")#ファイルの先頭の文字を上手く読み込めなかったバグがあったため改行を入れています。
 File.open("input.txt","r:utf-8") do |file|
     file.each_line do |tweet|
-        str=tweet.split(" ")#半角スペースで区切る
+        str=tweet.unicode_normalize(:nfkc).split(" ")#半角スペースで区切る
         str.insert(0," ")#処理のしやすさの観点から先頭に半角スペースを追加
         str.push(" ")#同様に末尾に半角スペースを追加
         str.each_cons(3) do |w1, w2, w3|
